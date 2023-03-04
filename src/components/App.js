62f370180610b60a06c59fb54730acfb38b1e13d
SYED RIYAZ <syedriyaz@192.168.1.10>
2023-03-04 19:37:01 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,28 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+  constructor(){
+    super();
+    this.state = {
+        name:"Jhon Doe",
+        enrollmentNo: "12345678", 
+        age: 34
+    };
+
+  }
+  onIncrementClickHandler=()=> {
+    this.setState({age:this.state.age+1});
+            };
+
+            render(){
+                return(
+<div class="ClassBasedComponent">
+<h1>Hello, my name is {this.state.name}</h1>
+                <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
+                <button onClick={this.onIncrementClickHandler}>Age Increment</button>
+</div>
+                );
+            }
 }
 
 
